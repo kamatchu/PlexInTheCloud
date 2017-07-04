538285ab37ee6e10327834027b71cdd0de6f9576
chrisanthropic <ctarwater@gmail.com>
2017-07-03 17:27:19 -0700

    add basic configs to get couchpotato post-processing working again

--- 03a-nzbtomedia.sh
+++ 03a-nzbtomedia.sh
@@ -34,6 +34,25 @@ cd "$here"
 cp /opt/nzbget/scripts/* /home/$username/nzbget/scripts/
 cp /home/$username/nzbget/scripts/autoProcessMedia.cfg.spec /home/$username/nzbget/scripts/autoProcessMedia.cfg
 
+## CouchPotato
+## Write CouchPotato API to nzbget.conf so it can send post-processing requests
+### Copy the api key from the CP config file
+cpAPI=$(cat /home/$username/.couchpotato/settings.conf | grep "api_key = ................................" | cut -d= -f 2)
+
+### Cut the single blank space that always gets added to the front of $cpAPI
+cpAPInew="$(sed -e 's/[[:space:]]*$//' <<<${cpAPI})"
+
+### Write the API key to nzbget.conf
+sed -i "s/^#cpsapikey=.*/cpsapikey=$cpAPInew/g" /home/$username/nzbget/scripts/nzbToCouchPotato.py
+
+# Basic defaults to get post-processing working
+sed -i 's/^#auto_update=.*/auto_update=1/g' /home/$username/nzbget/scripts/nzbToCouchPotato.py
+sed -i 's/^#cpsCategory=.*/cpsCategory=movies/g' /home/$username/nzbget/scripts/nzbToCouchPotato.py
+sed -i 's/^#cpsdelete_failed=.*/cpsdelete_failed=1/g' /home/$username/nzbget/scripts/nzbToCouchPotato.py
+sed -i 's/^#getSubs=.*/getSubs=1/g' /home/$username/nzbget/scripts/nzbToCouchPotato.py
+sed -i "s/^#subLanguages=.*/subLanguages=$openSubtitlesLang/g" /home/$username/nzbget/scripts/nzbToCouchPotato.py
+sed -i "s|^#cpswatch_dir=.*|cpswatch_dir=/home/$username/nzbget/completed/movies|g" /home/$username/nzbget/scripts/nzbToCouchPotato.py
+
 #######################
 # Permissions
 #######################
